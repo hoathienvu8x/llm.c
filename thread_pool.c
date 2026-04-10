9516fef4bd488bc4037950bf55f24cec4d65c0f0
Stanislav Fomichev <sdf@fomichev.me>
2026-04-10 13:28:05 -0700

    avoid ht
    Signed-off-by: Stanislav Fomichev <sdf@fomichev.me>

--- thread_pool.c
+++ thread_pool.c
@@ -1,8 +1,16 @@
 #include "thread_pool.h"
 
+#define _GNU_SOURCE
+#include <assert.h>
+#include <sched.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#endif
 
 /* No clear guidance on these, TODO - move to env/config? */
 #define MIN_FLOPS_THREADED  (16 * 1024 * 1024)
@@ -25,6 +33,46 @@ struct worker_arg {
 };
 
 static struct thread_pool g_tp;
+
+static int detect_physical_cores(void)
+{
+	int seen[4096] = {};
+	int ncpu = sysconf(_SC_NPROCESSORS_ONLN);
+	int count = 0;
+
+	for (int cpu = 0; cpu < ncpu; cpu++) {
+		char path[128];
+		int core_id, pkg = 0;
+		FILE *f;
+
+		snprintf(path, sizeof(path),
+			 "/sys/devices/system/cpu/cpu%d/topology/core_id", cpu);
+		f = fopen(path, "r");
+		if (!f)
+			return 0;
+		if (fscanf(f, "%d", &core_id) != 1) {
+			fclose(f);
+			return 0;
+		}
+		fclose(f);
+
+		snprintf(path, sizeof(path),
+			 "/sys/devices/system/cpu/cpu%d/topology/physical_package_id", cpu);
+		f = fopen(path, "r");
+		if (f) {
+			fscanf(f, "%d", &pkg);
+			fclose(f);
+		}
+
+		int key = pkg * 1024 + core_id;
+		assert(key >= 0 && key < (int)ARRAY_SIZE(seen));
+		if (!seen[key]) {
+			seen[key] = 1;
+			count++;
+		}
+	}
+	return count;
+}
 
 static void *worker(void *p)
 {
@@ -49,6 +97,10 @@ static void thread_pool_init(void)
 {
 	int ncpu = sysconf(_SC_NPROCESSORS_ONLN);
 	if (ncpu < 1) ncpu = 1;
+
+	int phys = detect_physical_cores();
+	if (phys > 0)
+		ncpu = phys;
 
 	g_tp.nthreads = ncpu;
 	g_tp.threads = calloc(ncpu, sizeof(pthread_t));
@@ -86,10 +138,10 @@ static int pick_nthreads(size_t m, size_t k, size_t n)
 
 	int by_flops = flops / MIN_FLOPS_PER_THREAD;
 	int nt = by_flops < n ? by_flops : n;
-	if (nt < m && m < nt)
-		nt = m;
-	if (nt > g_tp.nthreads) nt = g_tp.nthreads;
-	if (nt < 1) nt = 1;
+	if (nt > g_tp.nthreads)
+		nt = g_tp.nthreads;
+	if (nt < 1)
+		nt = 1;
 	return nt;
 }
 
